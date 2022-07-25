bb827b3fbddf10908c36cec74aa2163b8f5384cc
unknown <angelopc7@gmail.com>
2022-07-25 11:05:43 -0300

    Removendo permanecer conectado e alterar senha da page Login e inserindo favicon e logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,39 +5,29 @@ export function Login() {
                 <div className="w-[445px] h-[5px] m-auto mt-[10px] bg-green-500"></div>
                 <div className="px-[42px]">
                     <header className="flex justify-between pt-[14px]">
-                        <div className="w-[170px] h-[176px] bg-green-300"></div>
-                        <div className="w-[170px] h-[176px] bg-green-300"></div>
+                        <img src="/src/assets/logo_ifnmg_salinas.png" alt="Logo IFNMG" className="w-[170px] h-[176px]" />
+                        <img src="/src/assets/logo.svg" alt="Logo PortariasIF" className="w-[170px] h-[176px]" />
                     </header>
 
                     <form className="flex flex-col gap-4 w-full mt-6">
                         <input
-                            className="bg-gray-300 font-semibold text-base text-black rounded px-5 h-9"
+                            className="bg-gray-300 font-light text-base text-black rounded px-2 h-9"
                             type="text"
                             placeholder="Usuário"
                         />
                         <input
-                            className="bg-gray-300 font-semibold text-base text-black rounded px-5 h-9"
+                            className="bg-gray-300 font-light text-base text-black rounded px-2 h-9"
                             type="password"
                             placeholder="Senha"
                         />
 
-                        <label className="flex font-semibold text-base text-black items-center mt-4" >
-                            <input type="checkbox" className="w-[32px] h-[32px] mr-2" />
-                            Permanecer conectado
+                        <button
+                            type="submit"
+                            className="h-[38px] mt-2 bg-green-500 rounded font-medium text-base hover:bg-green-700 transition-colors disabled:opacity-50"
+                        >
+                            Acessar
+                        </button>
 
-                        </label>
-
-                        <div className="flex items-center justify-between">
-                            <a href="#" className="font-ligth text-sm text-black">Alterar senha</a>
-
-                            <button
-                                type="submit"
-                                className="w-[121px] h-[38px] mt-2 bg-green-500 rounded font-medium text-base hover:bg-green-700 transition-colors disabled:opacity-50"
-
-                            >
-                                Acessar
-                            </button>
-                        </div>
 
 
                     </form>
