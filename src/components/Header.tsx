a6ec2041219113e4154923c3e2c4bec99a53050e
unknown <angelopc7@gmail.com>
2022-07-26 19:34:25 -0300

    Implementação da Page Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,26 @@
 export function Header() {
     return (
         <div>
-            <header className="w-full h-[179px] flex flex-col justify-center px-16 bg-green-500">
-                <strong className="uppercase font-bold text-[32px]">Portarias</strong>
-                <strong className="font-bold text-[32px] mt-[-12px]">Instituto Federal do Norte de Minas</strong>
+            <header className="w-full h-[159px] flex flex-col justify-center px-48 bg-green-500">
+                <strong className="uppercase font-bold text-5xl">Portarias</strong>
+                <strong className="font-light text-2xl mt-[-4px]">Instituto Federal do Norte de Minas</strong>
 
 
 
             </header>
 
-            <nav className="flex w-full h-[52px] justify-center px-16 bg-green-300">
+            <nav className="flex h-[33px] justify-center items-center px-48 bg-green-300">
                 <ul className="flex justify-center flex-col lg:flex-row list-none lg:ml-auto">
                     <li className="nav-item">
-                        <a href="#" className="pl-16 flex items-center font-medium text-[32px] leading-snug hover:opacity-75">Home</a>
+                        <a href="#" className="pr-4 flex items-center font-medium text-xl leading-relaxed hover:opacity-75">Home</a>
                     </li>
 
                     <li className="nav-item">
-                        <a href="#" className="pl-16 flex items-center font-medium text-[32px] leading-snug hover:opacity-75">Cadastro</a>
+                        <a href="#" className="px-4 flex items-center font-medium text-xl leading-relaxed border-x border-gray-500 hover:opacity-75">Cadastro</a>
                     </li >
 
                     <li className="nav-item">
-                        <a href="#" className="pl-16 flex items-center font-medium text-[32px] leading-snug hover:opacity-75">Busca</a>
+                        <a href="#" className="pl-4 flex items-center font-medium text-xl leading-relaxed hover:opacity-75">Busca</a>
                     </li>
                 </ul>
             </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,28 @@ import { Header } from "../components/Header";
 export function Home() {
 
     return (
-        <Header />
+        <div className="flex flex-col min-h-screen">
+            <Header />
+
+            <main className="flex flex-1 px-48">
+                <aside className="flex flex-col w-[276px] px-[10px] bg-gray-200 mt-[54px] justify-center ">
+                    <span className="flex mt-[21px] font-light text-xl text-gray-500 justify-center">
+                        Recentes
+                    </span>
+                    <div className="flex-1 h-full border-t border-green-300">
+
+                    </div>
+                </aside>
+
+                <div className="flex-1 max-h-full mt-[82px] ml-[70px] border-t border-green-300">
+                    <span className="flex ml-[13px] mt-[10px] font-medium text-xl text-red-900">
+                        Próximas da Data de Encerramento
+                    </span>
+
+                </div>
+            </main>
+
+        </div>
+
     );
 }
